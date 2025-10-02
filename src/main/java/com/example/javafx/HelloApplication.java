e64f4028b41fa156bfe0a18666decf45ac801502
misury <sliminck@gmail.com>
2025-10-02 19:23:34 +0700

    empty

---
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -0,0 +1,32 @@
+package com.example.javafx;
+//изменения для мерж реквеста
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
